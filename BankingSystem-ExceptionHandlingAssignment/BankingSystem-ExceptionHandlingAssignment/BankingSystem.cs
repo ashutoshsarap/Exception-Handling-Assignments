c757763f45c61d61353dc14c5dc5335e9a4f380f
Ashutosh Sarap <151398603+ashutoshsarap@users.noreply.github.com>
2026-03-12 16:26:30 +0530

    Add files via upload

---
+++ BankingSystem-ExceptionHandlingAssignment/BankingSystem-ExceptionHandlingAssignment/BankingSystem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+public class BankAccount
+{
+
+    private string AccHolderName { get; set; }
+    private decimal CurrentBalance { get; set; } = 5000;
+
+    public BankAccount(string accHolderName, decimal currentBalance)
+
+    {
+        AccHolderName = accHolderName;
+        CurrentBalance = currentBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new InvalidAmountException("Deposit amount should be greater than 0 you are giving an input < 0");
+        }
+        CurrentBalance += amount;
+        Console.WriteLine($"{amount} deposited in your account");
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new InvalidAmountException("Deposit amount should be greater than 0 you are giving an input < 0");
+        }
+        if (amount > CurrentBalance)
+        {
+            throw new InsufficientBalanceException($"Insufficient balance, cannot withdraw {amount}");
+        }
+        CurrentBalance -= amount;
+        Console.WriteLine($"{amount} withdrawn from your account");
+    }
+
+    public void TransferToOtherAcc(decimal amount, BankAccount otherBankAcc)
+    {
+        if (amount <= 0) { throw new InvalidAmountException("Deposit amount should be greater than 0 you are giving an input < 0"); }
+        else if (amount > CurrentBalance)
+        {
+            throw new InsufficientBalanceException($"Insufficient balance, cannot withdraw {amount}");
+        }
+        else if (otherBankAcc == null)
+        {
+            throw new AccountNotFoundException("Account not found");
+        }
+        else
+        {
+            this.CurrentBalance -= amount;
+            otherBankAcc.CurrentBalance += amount;
+            Console.WriteLine($"{amount} transfered from {this.AccHolderName}'s account to {otherBankAcc.AccHolderName} account");
+        }
+    }
+
+}
+
+public class InvalidAmountException : Exception
+{
+    public InvalidAmountException() { }
+
+    public InvalidAmountException(string message) : base(message) { }
+
+    public InvalidAmountException(string message, Exception innerException) : base(message, innerException)
+    {
+
+    }
+}
+
+public class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException() { }
+    public InsufficientBalanceException(string message) : base(message) { }
+
+    public InsufficientBalanceException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
+
+public class AccountNotFoundException : Exception
+{
+
+    public AccountNotFoundException() { }
+
+    public AccountNotFoundException(string message) : base(message) { }
+
+    public AccountNotFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+}
---
+++ BankingSystem-ExceptionHandlingAssignment/BankingSystem-ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+
+    public static void Main()
+    {
+
+        //=============================
+        //BANKING SYSTEM
+        //=============================
+
+        BankAccount bankAccount1 = new BankAccount("Ashutosh", 3000);
+        BankAccount bankAccount2 = new BankAccount("Sam", 1000);
+        BankAccount bankAccount3 = null;
+
+
+        try
+        {
+            bankAccount1.Withdraw(500);
+            bankAccount1.Deposit(2000);
+            bankAccount1.TransferToOtherAcc(500, bankAccount2);
+            bankAccount1.TransferToOtherAcc(500, bankAccount3);
+        }
+        catch (InvalidAmountException invex)
+        {
+            throw new InvalidAmountException("Invalid amount", invex);
+            Console.WriteLine("caught");
+        }
+        catch (InsufficientBalanceException inbex)
+        {
+            Console.WriteLine(inbex.Message);
+        }
+        catch (AccountNotFoundException anfex)
+        {
+            Console.WriteLine(anfex.Message);
+        }
+
+    }
+
+}
